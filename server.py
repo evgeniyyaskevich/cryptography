2019a3095582901e9dd92fbfeed26915209dacf0
evgeniyyaskevich <evgeniy.yaskevich@yandex.by>
2020-10-08 11:34:07 +0300

    init flask app, add /login endpoint

---
+++ server.py
@@ -0,0 +1,27 @@
+import json
+import uuid
+import os
+import base64
+import rsa
+from flask import Flask, request
+from Crypto.Cipher import AES, PKCS1_OAEP
+
+app = Flask(__name__)
+
+@app.route('/login', methods = ['POST'])
+def login():
+    body = request.get_json()
+    #check user credentials here
+    try:
+        pub_key = rsa.PublicKey(int(body['n']), int(body['e']))
+    except:
+        return json.dumps({'result_msg': 'Error during parsing public key.'}), 401
+    session_key = rsa.randnum.read_random_bits(128)
+    cipher = rsa.encrypt(session_key, pub_key)
+    cipher_str = base64.b64encode(cipher).decode('utf8')
+    return json.dumps({'session_key': cipher_str}), 200
+
+if __name__ == '__main__':
+    app.run()
+    
+    
