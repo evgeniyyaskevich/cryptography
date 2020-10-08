bfc0eb3441cd7fe288b67c0c18c36a3d5b444541
evgeniyyaskevich <evgeniy.yaskevich@yandex.by>
2020-10-08 11:53:13 +0300

    Add request body and session key printing to server

--- server.py
+++ server.py
@@ -4,19 +4,20 @@ import os
 import base64
 import rsa
 from flask import Flask, request
-from Crypto.Cipher import AES, PKCS1_OAEP
 
 app = Flask(__name__)
 
 @app.route('/login', methods = ['POST'])
 def login():
-    body = request.get_json()
+    body = request.get_json(force=True)
+    print('Request body:', body)
     #check user credentials here
     try:
         pub_key = rsa.PublicKey(int(body['n']), int(body['e']))
     except:
-        return json.dumps({'result_msg': 'Error during parsing public key.'}), 401
+        return json.dumps({'result_msg': 'Error during parsing public key.'}), 400
     session_key = rsa.randnum.read_random_bits(128)
+    print('Session key:', session_key)
     cipher = rsa.encrypt(session_key, pub_key)
     cipher_str = base64.b64encode(cipher).decode('utf8')
     return json.dumps({'session_key': cipher_str}), 200
